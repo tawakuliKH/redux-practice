e22b82965eb5fbfabc7f3508a28c6fa9c811430f
tawakuliKH <tawakuli456@gmail.com>
2023-11-09 01:14:35 +0330

    First version of the app using redux

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+import { useSelector } from 'react-redux/es/hooks/useSelector';
+import { CartIcon } from '../icons';
+
+const Navbar = () => {
+  const { amount } = useSelector((store) => store.cart);
+
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3>redux toolkit</h3>
+        <div className="nav-container">
+          <CartIcon />
+          <div className="amount-container">
+            <p className="total-amount">{amount}</p>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
